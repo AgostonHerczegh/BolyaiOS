63174d58bf28069b98f91f2f2fe6732baf04efea
Herczegh.Agoston <herczegh.agoston@gmail.com>
2022-12-12 14:44:55 +0100

    Első verzió, üres program.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        String sor;
+        do {
 
+            Scanner sc = new Scanner(System.in);
+            sor = sc.nextLine();
+        }
+        while (sor != "exit");
     }
 }
